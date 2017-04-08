f780c0892f552f29c4185713c8181da05936fa23
Evan Black <me@evanpblack.com>
2017-04-08 18:07:01 -0400

    Add color.js

---
+++ color.js
@@ -0,0 +1,2 @@
+const COLORS = ["black","white","red","green","blue","yellow","magenta","aqua",
+    "#777","#345","#700","#070","#007","#770","#707","#077"];
--- config.js
+++ config.js
@@ -5,9 +5,6 @@ const
     TIME = 200,     // 2000 // milliseconds in timeout
 
     W = H = PIXELS/SIZE,  // # of cells in a row/column
-    CELLS = W * H,  // total cells
-    COLORS = ["black","white","red","green","blue","yellow","magenta","aqua",
-        "#777","#345","#700","#070","#007","#770","#707","#077"];
-
+    CELLS = W * H;  // total cells
 
 var studentFunctions = [];
