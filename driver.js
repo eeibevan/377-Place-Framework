48e0b3634dac436e514080e9822272902e214e28
Evan Black <me@evanpblack.com>
2017-04-08 16:56:34 -0400

    Add Driver To Execute studentFunctions

---
+++ driver.js
@@ -0,0 +1,49 @@
+function Driver(studentFunctions, db, asap, time) {
+    this.studentFunctions = studentFunctions;
+    this.db = db;
+    this._asap = asap || false;
+    this.time = time || TIME || 1000;
+    this._procId = 0;
+}
+
+Driver.prototype.tick = function () {
+    var arr = shuffle(this.studentFunctions);
+    var order = [];
+    for (var i = 0; i < arr.length; i++) {
+        arr[i]();
+        order.push(arr[i].name);
+    }
+    alert(order);
+    db.write();
+
+    if (this._asap)
+        this._procId = requestAnimationFrame(this.tick.bind(this));
+    else
+        this._procId = setTimeout(this.tick.bind(this), this.time);
+
+};
+
+Driver.prototype.start = function () {
+    if (this._asap)
+        this._procId = requestAnimationFrame(this.tick.bind(this));
+    else
+        this._procId = setTimeout(this.tick.bind(this), this.time);
+};
+
+Driver.prototype.stop = function () {
+    if (this._procId === 0)
+        return;
+
+    if (this._asap)
+        cancelAnimationFrame(this._procId);
+    else
+        clearTimeout(this._procId);
+
+    this._procId = 0;
+};
+
+Driver.prototype.switchMode = function () {
+    this.stop();
+    this._asap = !this._asap;
+    this.start();
+};
