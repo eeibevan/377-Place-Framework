ddb6910c9eb34fbd5f01b675ea12140c76666bf8
Evan Black <me@evanpblack.com>
2017-04-19 13:42:56 -0400

    Add Timer

--- driver.js
+++ driver.js
@@ -1,9 +1,11 @@
-function Driver(studentFunctions, asap, time, ticksCounter, statsBody) {
+function Driver(studentFunctions, asap, time, ticksCounter, statsBody, timer) {
     this.studentFunctions = studentFunctions;
     this._asap = asap || false;
     this.time = time || TIME || 1000;
     this._ticksCounter  = ticksCounter;
     this._statsTableBody = statsBody;
+    this._timerElem = timer;
+    this._startTime  = new Date();
     this._procId = 0;
     this._ticks = 0;
 }
@@ -23,6 +25,9 @@ Driver.prototype.tick = function () {
         this.printStats();
 
     this._ticks++;
+
+   if (this._timerElem)
+       setInterval(this._timerTick.bind(this), 1000);
 
     if (this._asap)
         this._procId = requestAnimationFrame(this.tick.bind(this));
@@ -61,6 +66,36 @@ Driver.prototype.stop = function () {
         clearTimeout(this._procId);
 
     this._procId = 0;
+};
+
+Driver.prototype._timerTick = function () {
+    var timeDiff = new Date() - this._startTime;
+    //strip the ms
+    timeDiff /= 1000;
+
+    var seconds = Math.round(timeDiff % 60);
+
+    timeDiff = Math.floor(timeDiff / 60);
+    var minutes = Math.round(timeDiff % 60);
+
+    timeDiff = Math.floor(timeDiff / 60);
+    var hours = Math.round(timeDiff % 24);
+
+    timeDiff = Math.floor(timeDiff / 24);
+    var days = timeDiff ;
+
+    var acc = '';
+    if (days > 0)
+        acc += 'Days: ' + days + ' ';
+    if (hours > 0)
+        acc += 'Hours: ' + hours + ' ';
+    if (minutes > 0)
+        acc += 'Minutes: ' + minutes + ' ';
+    if (seconds > 0)
+        acc += 'Seconds: ' + seconds;
+
+    if (this._timerElem)
+        this._timerElem.innerHTML = acc;
 };
 
 Driver.prototype.switchMode = function () {
